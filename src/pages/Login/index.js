51edfb6eaf1c6ba9ef25346651c81fee6e2c1d5a
felipe1181 <felipe1181jua@gmail.com>
2020-05-13 15:43:13 -0300

    feat: criar componentes e pagina de login, onde há usuário e senha, tratamento do formulário, e guardar sessão no localstorage do navegador,

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,83 @@
 import React from 'react';
+import { Row, Col, Form, Input, Button } from 'antd';
+import { useHistory } from 'react-router-dom';
+
+import './styles.css';
+
+import headStyle from '../../assets/Images/head_style.svg';
 
 const Login = () => {
-    return <div />;
+    const history = useHistory();
+
+    function handleSubmit(data) {
+        localStorage.setItem(
+            'user',
+            JSON.stringify({ usuario: data.username, senha: data.password })
+        );
+        history.push('/');
+    }
+
+    return (
+        <>
+            <Row className="container-login">
+                <Col
+                    className="content-login"
+                    xs={{ span: 22, offset: 1 }}
+                    sm={{ span: 20, offset: 2 }}
+                    md={{ span: 12, offset: 6 }}
+                    lg={{ span: 10, offset: 7 }}
+                    xl={{ span: 8, offset: 8 }}
+                >
+                    <Col span={24}>
+                        <Row>
+                            <Col span={12} />
+                            <Col span={12}>
+                                <img width="100%" alt="tes" src={headStyle} />
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col className="header-login" offset={4} span={16}>
+                        <span>
+                            <b>HANG</b>CARIRI
+                        </span>
+                    </Col>
+                    <Col offset={2} span={20}>
+                        <Form name="basic" onFinish={handleSubmit}>
+                            <Form.Item
+                                name="username"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Jardel da cu!',
+                                    },
+                                ]}
+                            >
+                                <Input placeholder="Usuário" />
+                            </Form.Item>
+
+                            <Form.Item
+                                name="password"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please input your password!',
+                                    },
+                                ]}
+                            >
+                                <Input.Password placeholder="Senha" />
+                            </Form.Item>
+
+                            <Form.Item>
+                                <Button type="primary" htmlType="submit">
+                                    Entra ai vai!
+                                </Button>
+                            </Form.Item>
+                        </Form>
+                    </Col>
+                </Col>
+            </Row>
+        </>
+    );
 };
 
 export default Login;
